3e6b2bfbc762eb265ff556a716fd02b0109e019c
Granada0555 <114270479+Granada0555@users.noreply.github.com>
2026-02-16 21:44:11 -0500

    Refactor SeparateElements to use stacks for separation
    Refactor SeparateElements method to initialize included and excluded stacks and separate elements into them based on sequence membership.

--- TestMethods.cs
+++ TestMethods.cs
@@ -2,10 +2,69 @@
 {
     internal class TestMethods
     {
-        public static void SeparateElements(Queue<int> input, out Stack<int> included, out Stack<int> excluded)
+       public static void SeparateElements(Queue<int> input, out Stack<int> included, out Stack<int> excluded)
         {
-            included = null;
-            excluded = null;
+            included = new Stack<int>();
+            excluded = new Stack<int>();
+
+            int size = input.Count;
+
+            int[] tempIncluded = new int[size];
+            int[] tempExcluded = new int[size];
+
+            int countIncluded = 0;
+            int countExcluded = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                int number = input.Dequeue();
+
+                if (BelongsToSequence(number))
+                {
+                    tempIncluded[countIncluded++] = number;
+                }
+                else
+                {
+                    tempExcluded[countExcluded++] = number;
+                }
+            }
+
+            // Insertar en orden inverso para mantener el orden original
+            for (int i = countIncluded - 1; i >= 0; i--)
+            {
+                included.Push(tempIncluded[i]);
+            }
+
+            for (int i = countExcluded - 1; i >= 0; i--)
+            {
+                excluded.Push(tempExcluded[i]);
+            }
+        }
+
+        private static bool BelongsToSequence(int number)
+        {
+            if (number == 0)
+                return false;
+
+            int absolute = number < 0 ? -number : number;
+
+            int root = 1;
+
+            while (root * root < absolute)
+            {
+                root++;
+            }
+
+            if (root * root != absolute)
+                return false;
+
+            if (root % 2 != 0 && number < 0)
+                return true;
+
+            if (root % 2 == 0 && number > 0)
+                return true;
+
+            return false;
         }
 
         public static List<int> GenerateSortedSeries(int n) => null;
